2bd4cfa380c840dcfd317b5dee2feed17d16ac62
rredjane <rajabaly.redjane@soprasteria.com>
2024-05-24 19:46:11 +0200

    second commit

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import all_product from '../Components/Assets/all_product';
+import { ShopContext } from '../Context/ShopContext';
 
 const Product = () => {
+    const {all_product}= useContext(ShopContext)
     return (
         <div>
             
