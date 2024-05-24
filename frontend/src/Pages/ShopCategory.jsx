093b9358e4d1215148743a77c245c52c6bc346b2
rredjane <rajabaly.redjane@soprasteria.com>
2024-05-24 18:18:42 +0200

    first commit

---
+++ frontend/src/Pages/ShopCategory.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import "./CSS/ShopCategory.css";
+import { ShopContext } from '../Context/ShopContext';
+
+const ShopCategory = (props) => {
+    const { all_product } = useContext(ShopContext);  
+    return (
+        <div className='shop-category'>
+            <img src={props.banner} alt="" />
+            <div className="shop-category-indexSort">
+               <p>
+                <span>showing 1-12</span>
+               </p>
+            </div>
+        </div>
+    );
+};
+
+export default ShopCategory;
