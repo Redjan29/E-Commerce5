f233c4f03a8cc4e5b90d583853f037a10c2e90c8
rredjane <rajabaly.redjane@soprasteria.com>
2024-05-24 21:52:29 +0200

    Thrid commit

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -1,22 +1,18 @@
-import React from 'react';
-import './Item.css'
+import React from "react";
+import "./Item.css";
+import { Link } from "react-router-dom";
 
 const Item = (props) => {
   return (
-    <div className='item'>
-      <img src={props.image} alt="" />
+    <div className="item">
+      <Link to={`/product/${props.id}`}><img src={props.image} alt="" />
+      </Link>
       <p>{props.name}</p>
       <div className="item-prices">
+        <div className="item-price-new">${props.new_price}</div>
 
-      <div className='item-price-new'>
-        ${props.new_price}
+        <div className="item-price-old">${props.old_price}</div>
       </div>
-
-      <div className='item-price-old'>
-      ${props.old_price}
-      </div>
-
-    </div>
     </div>
   );
 };
